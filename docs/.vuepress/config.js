dbefbf31fe5609823db265157f3f9dcd23c422db
hubin <243194995@qq.com>
2022-03-25 16:36:42 +0800

    调整目录结构新增低代码解决方案收录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,8 +28,22 @@ module.exports = {
           { text: '主题初衷与诞生', link: '/pages/52d5c3/' },
           { text: '介绍', link: '/pages/a2f161/' },
           { text: '快速上手', link: '/pages/793dcb/' },
+        ]
+      },
+      {
+        text: '安全', link: '/pages/9xd005/', items: [
           { text: '单点登录SSO', link: '/pages/9xd005/' },
+          { text: '图片验证码', link: '/pages/9xd006/' }
+        ]
+      },
+      {
+        text: '监控', link: '/pages/9xd010/', items: [
           { text: '系统监控', link: '/pages/9xd010/' }
+        ]
+      },
+      {
+        text: '低代码', link: '/pages/a10ce8/', items: [
+          { text: '开源低代码解决方案', link: '/pages/a10ce8/' }
         ]
       },
       {
